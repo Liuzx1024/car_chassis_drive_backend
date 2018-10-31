78fd7e0aa4ea8d93396f73d7919006c6751e8513
“chfanghr” <chfanghr@gmail.com>
2018-10-31 18:18:26 +0800

    Update

--- cmd/raspi_pin_test/main.go
+++ cmd/raspi_pin_test/main.go
@@ -10,5 +10,26 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(pin.DigitalRead())
+	value, err := pin.DigitalRead()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(value)
+	err = pin.SetPinMode(raspi.OUT)
+	if err != nil {
+		panic(err)
+	}
+	err = pin.DigitalWrite(raspi.HIGH)
+	if err != nil {
+		panic(err)
+	}
+	value, err = pin.DigitalRead()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(value)
+	err = raspi.Raspi.UnexportPin(3)
+	if err != nil {
+		panic(err)
+	}
 }
