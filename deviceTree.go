6e76daa4aa625669248267d2c1e508dda6de07f4
“chfanghr” <chfanghr@gmail.com>
2018-11-07 14:04:02 +0800

    Update

--- deviceTree.go
+++ deviceTree.go
@@ -2,21 +2,57 @@ package backend
 
 import (
 	"backend/uuid"
+	"errors"
 	"sync"
 )
 
 type node struct {
+	mutex      *sync.Mutex
+	childNodes []uuid.UUID
 }
 
-type _nodes struct {
-	nodes map[uuid.UUID]node
-	mutex *sync.Mutex
-}
-
-func (_this *_nodes) NewNode() uuid.UUID {
-	return uuid.UUID{}
-}
+var nodes map[uuid.UUID]*node
+var nodesMutex *sync.RWMutex
 
 func init() {
+	nodes = make(map[uuid.UUID]*node)
+	nodesMutex = new(sync.RWMutex)
+}
 
+func NewNode() (uuid.UUID, error) {
+	nodesMutex.Lock()
+	defer nodesMutex.Unlock()
+
+	for {
+		newUUID, err := uuid.NewV4()
+		if err != nil {
+			return uuid.UUID{}, err
+		}
+		if _, ok := nodes[newUUID]; ok != false {
+			continue
+		}
+		ptr := &node{
+			childNodes: []uuid.UUID{},
+			mutex:      new(sync.Mutex),
+		}
+		nodes[newUUID] = ptr
+		return newUUID, nil
+	}
+}
+
+func DeleteNode(uuid uuid.UUID) error {
+	nodesMutex.Lock()
+	defer nodesMutex.Unlock()
+
+	if ptr, ok := nodes[uuid]; ok == true {
+		ptr.mutex.Lock()
+		defer ptr.mutex.Unlock()
+
+		if len(ptr.childNodes) != 0 {
+			return errors.New("DeleteNode failed")
+		}
+		delete(nodes, uuid)
+		return nil
+	}
+	return errors.New("Don't have such node")
 }
